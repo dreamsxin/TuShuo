8c64050bc8e5f886813a304e5b9579003e43499b
Dreamsxin <dreamsxin@126.com>
2015-08-05 16:37:09 +0800

    增加搜索主题页面

--- client/www/js/tushuo.js
+++ client/www/js/tushuo.js
@@ -6,6 +6,15 @@ var tushuo = {
 		window.localStorage.setItem('email', email);
 		window.localStorage.setItem('password', password);
 		window.location.href = 'index.html';
+	},
+	search: function(){
+		navigator.notification.prompt('请输入您感兴趣主题的名称或关键字', function(results){
+			window.location.href = 'search.html?' + results.input1;
+		},
+		'搜索主题',
+		['确定'],
+		'今日热门'
+		);
 	}
 };
 
